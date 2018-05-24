a99870d1b10fda98466933c42227772fac7a0209
gdcp <gdcp@m25.local>
2018-05-24 11:39:03 +0800

    create

--- SHAKESPEED/ViewController.swift
+++ SHAKESPEED/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
-
+import CoreMotion
 class ViewController: UIViewController {
-
+    var manager : CMMotionManager?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       manager = CMMotionManager()
+        manager?.startAccelerometerUpdates()
+        manager?.startGyroUpdates()
+        let timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(update),userInfo : nil,repeats: true)
     }
-
+    func update(){
+        print(manager?.accelerometerData?.acceleration.x)
+        print(manager?.accelerometerData?.acceleration.y)
+        print(manager?.gyroData?.rotationRate.z)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
